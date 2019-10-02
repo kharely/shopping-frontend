804170fce073ad7e82e889103a64c45275e1c110
kharely <kadelyta1996@gmail.com>
2019-10-01 20:40:24 -0500

    Mi first commit of frontend

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,96 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="row justify-content-center">
+    <div class="col-10 mt-md-5 ">
+      <div class="row justify-content-center">
+        <div class="col">
+          <table class="table">
+            <thead class="table-success">
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Price</th>
+                <th scope="col-1"></th>
+              </tr>
+            </thead>
+            <tbody>
+            <tr v-for="item in items" v-bind:key="item.CODE">
+              <th scope=row>{{item.NAME}}</th>
+              <th>$ {{item.PRICE}}</th>
+              <th><button class="btn btn-success mr-1" @click="increment">
+                <i class="fas fa-plus-circle"></i>
+              </button>
+              <button class="btn btn-success" @click="decrement(item.CODE)">
+                <i class="fas fa-minus-circle"></i>
+              </button>
+              </th>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <button class="btn btn-primary">Enviar</button>
+      <div class="row justify-content-center">
+        <div class="col-12 col-md-5">
+          <h3>Items</h3>
+          <div class="row">
+            <p>{{shopping}}</p>
+          </div>
+        </div>
+        <div class="col-12 col-md-5">
+          <h3>Checkout</h3>
+          <table class="table">
+            <thead class="table-success">
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Price</th>
+              </tr>
+            </thead>
+            <tbody>
+            <tr v-for="item in items" v-bind:key="item.CODE">
+              <th scope=row>{{item.NAME}}</th>
+              <th>$ {{item.PRICE }}</th>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+const API_URL = 'http://localhost:3000';
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
+  data: () => ({
+    error: '',
+    items: [],
+    shopping: {
+      pants: 0,
+      hat: 0,
+      tshirt: 0,
+    },
+  }),
+
+  mounted() {
+    fetch(API_URL)
+      .then(response => response.json())
+      .then((response) => {
+        this.items = response;
+      });
+  },
+
+  methods: {
+    increment(name) {
+      this.shopping[name.toLowerCase()] += 1;
+    },
+    decrement(name) {
+      const nameLower = name.toLowerCase();
+      if (this.shopping[nameLower].counter > 0) {
+        this.shopping[nameLower] -= 1;
+      }
+    },
   },
 };
+
 </script>
